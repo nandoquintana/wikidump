d053430c162adf393e16d668449759aaa0fa571e
Diego Caro <diegocaro@gmail.com>
2011-07-13 22:01:18 -0400

    excluding articles that are categories

--- examples/articles_by_category.py
+++ examples/articles_by_category.py
@@ -9,6 +9,7 @@ sys.path.append('../src/')
 from wikidump.utils import load_dumps, find_dumps
 from wikidump.model import Dump
 
+from wikidump.lang import category_identifier 
 
 def main():
   logging.basicConfig(level=logging.DEBUG)
@@ -26,17 +27,23 @@ def main():
   cats = dump.categories
   get_page = dump.get_page_by_index
 
+  cat_spacename = category_identifier[options.lang]
+
   if options.output == 'csv':
     writer = csv.writer(sys.stdout,quoting=csv.QUOTE_ALL)
     for c in sorted(cats, key=lambda x:len(cats[x]), reverse=True):
       for a in cats[c]:
-        writer.writerow((c, get_page(a).title))
+        title = get_page(a).title
+        if title.find(cat_spacename) == -1:
+          writer.writerow((c, title))
   
   else:
     for c in sorted(cats, key=lambda x:len(cats[x]), reverse=True):
       print "%s:" % (c)
       print "# %d articles" % (len(cats[c]))
       for a in cats[c]:
-        print "- %s" %( get_page(a).title )
+        title = get_page(a).title
+        if title.find(cat_spacename) == -1:
+          print "- %s" %( get_page(a).title )
 
 main()
